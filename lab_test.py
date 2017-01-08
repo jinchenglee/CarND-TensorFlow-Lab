198d5ea6619020ca6427732d81bcf2b3c2c27c3c
jinchenglee <jincheng.li@gmail.com>
2017-01-07 20:10:53 -0800

    Clean up lab.py, merging test accuracy into same session run. Split a lab_test.py to run test set separately if desired.

--- lab_test.py
+++ lab_test.py
@@ -94,18 +94,9 @@ accuracy = tf.reduce_mean(tf.cast(is_correct_prediction, tf.float32))
 
 print('Accuracy function created.')
 
-
-
-# In[11]:
-
-# TODO: Find the best parameters for each configuration
-epochs = 200
-batch_size = 1000
+# Learning rate
 learning_rate = 0.03
 
-
-
-### DON'T MODIFY ANYTHING BELOW ###
 # Gradient Descent
 optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(loss)    
 #optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)    
@@ -118,76 +109,17 @@ save_file = 'model.ckpt'
 saver = tf.train.Saver()
 
 
-# The accuracy measured against the validation set
-validation_accuracy = 0.0
-
-# Measurements use for graphing loss and accuracy
-log_batch_step = 50
-batches = []
-loss_batch = []
-train_acc_batch = []
-valid_acc_batch = []
+### DON'T MODIFY ANYTHING BELOW ###
+# The accuracy measured against the test set
+test_accuracy = 0.0
 
 with tf.Session() as session:
-    session.run(init)
-    batch_count = int(math.ceil(len(train_features)/batch_size))
+    
+    saver.restore(session, '/home/vitob/git_repositories/CarND-TensorFlow-Lab/model.ckpt')
 
-    for epoch_i in range(epochs):
-        
-        # Progress bar
-        batches_pbar = tqdm(range(batch_count), desc='Epoch {:>2}/{}'.format(epoch_i+1, epochs), unit='batches')
-        
-        # The training cycle
-        for batch_i in batches_pbar:
-            # Get a batch of training features and labels
-            batch_start = batch_i*batch_size
-            batch_features = train_features[batch_start:batch_start + batch_size]
-            batch_labels = train_labels[batch_start:batch_start + batch_size]
+    # Check accuracy against Test data
+    test_accuracy = session.run(accuracy, feed_dict=test_feed_dict)
 
-            # Run optimizer and get loss
-            _, l = session.run(
-                [optimizer, loss],
-                feed_dict={features: batch_features, labels: batch_labels})
-
-            # Log every 50 batches
-            if not batch_i % log_batch_step:
-                # Calculate Training and Validation accuracy
-                training_accuracy = session.run(accuracy, feed_dict=train_feed_dict)
-                validation_accuracy = session.run(accuracy, feed_dict=valid_feed_dict)
-
-                # Log batches
-                previous_batch = batches[-1] if batches else 0
-                batches.append(log_batch_step + previous_batch)
-                loss_batch.append(l)
-                train_acc_batch.append(training_accuracy)
-                valid_acc_batch.append(validation_accuracy)
-
-        # Check accuracy against Validation data
-        validation_accuracy = session.run(accuracy, feed_dict=valid_feed_dict)
-
-        # Check accuracy against Test data
-        test_accuracy = session.run(accuracy, feed_dict=test_feed_dict)
-  
-    # Save the network
-    saver.save(session, save_file)
-
-loss_plot = plt.subplot(211)
-loss_plot.set_title('Loss')
-loss_plot.plot(batches, loss_batch, 'g')
-loss_plot.set_xlim([batches[0], batches[-1]])
-acc_plot = plt.subplot(212)
-acc_plot.set_title('Accuracy')
-acc_plot.plot(batches, train_acc_batch, 'r', label='Training Accuracy')
-acc_plot.plot(batches, valid_acc_batch, 'x', label='Validation Accuracy')
-acc_plot.set_ylim([0, 1.0])
-acc_plot.set_xlim([batches[0], batches[-1]])
-acc_plot.legend(loc=4)
-plt.tight_layout()
-plt.show()
-
-print('Training accuracy at {}'.format(training_accuracy))
-print('Validation accuracy at {}'.format(validation_accuracy))
 assert test_accuracy >= 0.80, 'Test accuracy at {}, should be equal to or greater than 0.80'.format(test_accuracy)
 print('Nice Job! Test Accuracy is {}'.format(test_accuracy))
-
 
